99202dd79e454794a212c293ae58c32b0b7cee11
Vladislav Ledashchev <ybhpg@icloud.com>
2021-12-05 15:21:09 +0300

    • Changed the role from 'client' to 'collector' • Changed the first SQL query to match the table headings • Added the same style to tables in all SQL queries' results • Improved the look of the docx file

--- app/scen_query/sql/query1.sql
+++ app/scen_query/sql/query1.sql
@@ -1,5 +1,5 @@
-SELECT sum(total_cost) as sum,
-    user_id
+SELECT user_id,
+    sum(total_cost) as sum
 FROM reserved_product_payment.order
 WHERE month(order_date) = '$month'
     AND year(order_date) = '$year'
